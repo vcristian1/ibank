35c83e0f4bd135ec49c22db9a3e61ae92c53abca
Cristian Vargas <cristian.v0223@gmail.com>
2023-03-29 17:02:11 -0500

    added margin left to Testimonials.jsx

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -2,10 +2,10 @@ import { feedback } from "../constants";
 import styles from "../style";
 
 const Testimonials = () => (
-  <section id="clients" className={`${styles.paddingY} ${styles.flexCenter} flex-col relative `}>
+  <section id="clients" className={`${styles.paddingY} ${styles.flexCenter} flex-col relative`}>
     <div className="absolute z-[0] w-[60%] h-[60%] -right-[50%] rounded-full blue__gradient bottom-40" />
 
-    <div className="w-full flex justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relative z-[1]">
+    <div className="w-full flex justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relative z-[1] md:ml-[130px]">
       <h2 className={styles.heading2}>
         What People are <br className="sm:block hidden" /> saying about us
       </h2>
