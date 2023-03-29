b7faa3fcb381215e7ab8d3100d9fb8d12dd49b30
Cristian Vargas <cristian.v0223@gmail.com>
2023-03-29 15:20:50 -0500

    created new component FeaturesCard that is rendered in Business.jsx

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -3,6 +3,7 @@ import styles, { layout } from "../style";
 import Button from "./Button";
 
 const Business = () =>  (
+
   <section id="features" className="flex md:flex-row flex-col sm:py-16 py-6 ml-16">
     <div className={`flex-1 ${styles.flexStart} flex-col`}>
       <h2 className="font-poppins font-semibold xs:text-[48px] text-[40px] text-white xs:leading-[76.8px] leading-[66.8px] w-full">
@@ -16,6 +17,12 @@ const Business = () =>  (
       </p>
 
       <Button styles={`mt-10`} />
+    </div>
+
+    <div className={`${layout.sectionImg} flex-col`}>
+      {features.map((feature, index) => (
+        <FeaturesCard />
+      ))}
     </div>
 
   </section>
---
+++ src/components/FeaturesCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FeaturesCard = () => {
+  return (
+    <div>FeaturesCard</div>
+  )
+}
+
+export default FeaturesCard
