577e6ec24691578d1b08251f30fdb38821fce2fd
Cristian Vargas <cristian.v0223@gmail.com>
2023-03-29 12:13:56 -0500

    created styles.js in src directory

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 const App = () => {
   return (
     <div className='bg-primary w-full overflow-hidden'>
-      <h1>Hello World</h1>
+       {/* Container wrapping Navbar */}
+      <div>
+
+      </div>
     </div>
   )
 }
