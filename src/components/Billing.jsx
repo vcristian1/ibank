a1398ca34657263ecd753c302d79d4031c8a42ff
Cristian Vargas <cristian.v0223@gmail.com>
2023-03-29 16:44:21 -0500

    removed motion.div and replaced with div so the gradient can render behind the bill image

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -5,23 +5,14 @@ import { motion } from "framer-motion";
 const Billing = () => {
   return (
     <section id="product" className={`${layout.sectionReverse} md:ml-16 ml-6`}>
-      <motion.div 
-       className={layout.sectionImgReverse}
-       initial="hidden"
-       whileInView="visible"
-       viewport={{ once: true, amount: 0.5 }}
-       transition={{ delay: 1, duration: 0.5 }}
-       variants={{
-          hidden: { opacity: 0, x: -50 },
-          visible: { opacity: 1, x: 0 },
-       }} 
-      >
-        <img src={bill} alt="billing" className="w-[100%] h-[100%] relative z-[5]"/>
+      <div className={layout.sectionImgReverse}>
+        <img src={bill} alt="billing" className="w-[100%] h-[100%] relative z-[5]" />
 
-        <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white_gradient" />
-        <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white_gradient" />
-
-      </motion.div>
+        {/* gradient start */}
+        <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient" />
+        <div className="absolute z-[0] w-[50%] h-[50%] -left-1/2 bottom-0 rounded-full pink__gradient" />
+        {/* gradient end */}
+      </div>
 
       <motion.div 
        className={layout.sectionInfo}
