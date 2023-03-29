c8bebb736d5edba2d78d79d14288f859b76edda2
Cristian Vargas <cristian.v0223@gmail.com>
2023-03-29 14:10:52 -0500

    added css classes to the parent container of the robot graphic to adjust the gradients and how they render

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -29,7 +29,7 @@ const Hero = () => {
         <p className={`${styles.paragraph} ml-2`}>Our team of experts use a methodology to identify the credit cards most likely to fit your needs. We examine annual percentage rates, annual fees.</p>
       </div>
 
-      <div>
+      <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
         <img src={robot} alt="billing" className="w-[100%] h-[100%] relative z-[5]"/>
         {/* gradient start */}
         <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
