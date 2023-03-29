c6ea4f9077bafa0edffdcbf15059fb7ce0dc481d
Cristian Vargas <cristian.v0223@gmail.com>
2023-03-29 16:05:08 -0500

    added graphic to billing component

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import { apple, bill, google } from "../assets";
+import styles, { layout } from "../style";
 
 const Billing = () => {
   return (
-    <div>Billing</div>
+    <section id="product" className={layout.sectionReverse}>
+      <div className={layout.sectionImgReverse}>
+        <img src={bill} alt="billing" className="w-[100%] h-[100%] relative z-[5]"/>
+
+        <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white_gradient" />
+        <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white_gradient" />
+
+      </div>
+    </section>
   )
 }
 
