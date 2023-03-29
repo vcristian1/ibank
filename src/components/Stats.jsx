a10672a73e81a7e6d1f2058c268a04b001e38c9e
Cristian Vargas <cristian.v0223@gmail.com>
2023-03-29 14:43:04 -0500

    imported framer motion, and added motion.div to Stats.jsx, and its now complete

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,18 +1,45 @@
 import React from 'react'
 import { stats } from "../constants";
 import styles from "../style";
+import { motion } from "framer-motion";
 
 const Stats = () => {
   return (
     <section className={`${styles.flexCenter} flex-row flex-wrap sm:mb-20 mb-6`}>
       {stats.map((stat) => (
-        <div key={stat.id} className="text-white">
-          <h4>{stat.value}</h4>
-          <p>{stat.title}</p>
+        <div 
+         key={stat.id} 
+         className={`flex flex-1 justify-start items-center m-3 ml-14`}
+        >
+          <motion.div
+           initial="hidden"
+           whileInView="visible"
+           viewport={{ once: true, amount: 0.5 }}
+           transition={{ delay: 0.5, duration: 0.5 }}
+           variants={{
+              hidden: { opacity: 0, x: -50 },
+              visible: { opacity: 1, x: 0 },
+           }} 
+          >
+            <h4 className='font-poppins font-semibold xs:text-[40px] text-[30px] xs:leading-[53px] leading-[43px] text-white'>{stat.value}</h4>
+          </motion.div>
+
+          <motion.div
+           initial="hidden"
+           whileInView="visible"
+           viewport={{ once: true, amount: 0.5 }}
+           transition={{ delay: 1, duration: 0.5 }}
+           variants={{
+              hidden: { opacity: 0, x: -50 },
+              visible: { opacity: 1, x: 0 },
+           }} 
+          >
+            <p className='font-poppins font-normal xs:text-[20px] text-[15px] xs:leading-[26px] leading-[21px] text-gradient uppercase ml-3'>{stat.title}</p>
+          </motion.div>
         </div>
       ))}
     </section>
   )
 }
 
-export default Stats
+export default Stats;
