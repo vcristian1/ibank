a28dff2d6dbc168b27646776161ad36d7746249e
Cristian Vargas <cristian.v0223@gmail.com>
2023-03-29 17:11:59 -0500

    completed CTA component, adjusted margin left in Testimonials component

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -6,7 +6,7 @@ const Testimonials = () => (
   <section id="clients" className={`${styles.paddingY} ${styles.flexCenter} flex-col relative`}>
     <div className="absolute z-[0] w-[60%] h-[60%] -right-[50%] rounded-full blue__gradient bottom-40" />
 
-    <div className="w-full flex justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relative z-[1] md:ml-[130px]">
+    <div className="w-full flex justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relative z-[1] md:ml-[130px] ml-12">
       <h2 className={styles.heading2}>
         What People are <br className="sm:block hidden" /> saying about us
       </h2>
@@ -18,7 +18,7 @@ const Testimonials = () => (
       </div>
     </div>
 
-    <div className="flex flex-wrap sm:justify-start justify-center w-full feedback-container relative z-[1] md:ml-[130px]">
+    <div className="flex flex-wrap sm:justify-start justify-center w-full feedback-container relative z-[1] md:ml-[130px] ml-12">
       {feedback.map((card) => <FeedbackCard key={card.id} {...card} />)}    
     </div>
   </section>
