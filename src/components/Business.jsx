0eabdadf3b12c86965853a652d65cecfbf43e3ba
Cristian Vargas <cristian.v0223@gmail.com>
2023-03-29 15:31:05 -0500

    adjusted margin on small screens and medium screens

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -2,9 +2,27 @@ import { features } from "../constants";
 import styles, { layout } from "../style";
 import Button from "./Button";
 
+const FeatureCard = ({ icon, title, content, index }) => {
+
+  <div className={`flex flex-row p-6 rounded-[20px] ${index !== features.length - 1 ? "mb-6" : "mb-0"} feature-card`}>
+    <div className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}>
+      <img src={icon} alt="star" className="w-[50%] h-[50%] object-contain" />
+    </div>
+    <div className="flex-1 flex flex-col ml-3">
+      <h4 className="font-poppins font-semibold text-white text-[18px] leading-[23.4px] mb-1">
+        {title}
+      </h4>
+      <p className="font-poppins font-normal text-dimWhite text-[16px] leading-[24px]">
+        {content}
+      </p>
+    </div>
+  </div>
+
+}
+
 const Business = () =>  (
 
-  <section id="features" className="flex md:flex-row flex-col sm:py-16 py-6 ml-16">
+  <section id="features" className="flex md:flex-row flex-col sm:py-16 py-6 md:ml-14 ml-6">
     <div className={`flex-1 ${styles.flexStart} flex-col`}>
       <h2 className="font-poppins font-semibold xs:text-[48px] text-[40px] text-white xs:leading-[76.8px] leading-[66.8px] w-full">
         You do the business, <br className="sm:block hidden" /> we’ll handle
@@ -21,7 +39,9 @@ const Business = () =>  (
 
     <div className={`${layout.sectionImg} flex-col`}>
       {features.map((feature, index) => (
-        <FeaturesCard />
+        <FeatureCard 
+         key={feature.id} {...feature} index={index}
+        />
       ))}
     </div>
 
--- src/components/FeaturesCard.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const FeaturesCard = () => {
-  return (
-    <div>FeaturesCard</div>
-  )
-}
-
-export default FeaturesCard
